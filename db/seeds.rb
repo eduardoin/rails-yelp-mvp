a789a7735d3a6714ab5aa689102493b45a0a46f4
Eduardo Insaurriaga <eduardoin@gmail.com>
2019-02-27 23:52:30 -0300

    Basic Routing and Controllers finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  seed = Restaurant.new(
+    name:         Faker::Restaurant.name,
+    address:      Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category:     ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+  seed.save
+end
